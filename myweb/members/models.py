fe7a04008ed88147abdd8621c99217d2b3f3b22f
nirawit2550 <6529011019@cdti.ac.th>
2024-09-16 16:10:02 +0700

    update

--- myweb/members/models.py
+++ myweb/members/models.py
@@ -3,5 +3,4 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    joined_date = models.DateField()
+   
